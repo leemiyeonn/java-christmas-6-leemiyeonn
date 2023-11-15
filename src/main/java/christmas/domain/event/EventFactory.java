2a54f8fd9d25e90454d711f2c3b88e2d9920712a
leemiyeon <lemingul7@gmail.com>
2023-11-15 15:42:54 +0900

    refactor: 이벤트 적용 로직 리팩토링 - 이벤트 정보 enum 으로 정의

--- src/main/java/christmas/domain/event/EventFactory.java
+++ src/main/java/christmas/domain/event/EventFactory.java
@@ -1,5 +1,6 @@
 package christmas.domain.event;
 
+import christmas.domain.constants.event.EventConstants;
 import christmas.domain.constants.event.EventType;
 import christmas.domain.event.discount.strategy.ChristmasDiscountStrategy;
 import christmas.domain.event.discount.strategy.DayOfWeekDiscountStrategy;
@@ -15,33 +16,57 @@ public class EventFactory {
     public static List<Event> createEvents() {
         List<Event> events = new ArrayList<>();
 
-        EventPeriod christmasEventPeriod = new EventPeriod(1, 25);
-        EventPeriod decemberEventPeriod = new EventPeriod(1, 31);
+        EventPeriod christmasEventPeriod = new EventPeriod(EventConstants.CHRISTMAS_EVENT_START_DAY.getValue(), EventConstants.CHRISTMAS_EVENT_END_DAY.getValue());
+        EventPeriod decemberEventPeriod = new EventPeriod(EventConstants.DECEMBER_FIRST_DAY.getValue(), EventConstants.DECEMBER_LAST_DAY.getValue());
 
-        Set<Integer> weekdayIndex = Set.of(0,1,2,3,4);
-        Set<Integer> weekendIndex = Set.of(5,6);
+        Set<Integer> weekdayIndex = Set.of(
+                EventConstants.SUNDAY_INDEX.getValue(),
+                EventConstants.MONDAY_INDEX.getValue(),
+                EventConstants.TUESDAY_INDEX.getValue(),
+                EventConstants.WEDNESDAY_INDEX.getValue(),
+                EventConstants.THURSDAY_INDEX.getValue()
+        );
 
-        Set<Integer> specialDates = Set.of(3,10,17,24,25,31);
+        Set<Integer> weekendIndex = Set.of(
+                EventConstants.FRIDAY_INDEX.getValue(),
+                EventConstants.SATURDAY_INDEX.getValue()
+        );
+
+        Set<Integer> specialDates = Set.of(
+                EventConstants.SPECIAL_DATE_ONE.getValue(),
+                EventConstants.SPECIAL_DATE_TWO.getValue(),
+                EventConstants.SPECIAL_DATE_THREE.getValue(),
+                EventConstants.SPECIAL_DATE_FOUR.getValue(),
+                EventConstants.SPECIAL_DATE_FIVE.getValue(),
+                EventConstants.SPECIAL_DATE_SIX.getValue()
+        );
 
         events.add(new Event(EventType.CHRISTMAS_DISCOUNT, christmasEventPeriod,
-                new ChristmasDiscountStrategy(1, 1_000, 100),null));
+                new ChristmasDiscountStrategy(
+                        EventConstants.CHRISTMAS_EVENT_START_DAY.getValue(),
+                        EventConstants.CHRISTMAS_DISCOUNT_START_AMOUNT.getValue(),
+                        EventConstants.CHRISTMAS_DISCOUNT_INCREMENT.getValue()), null));
 
         events.add(new Event(EventType.WEEKDAY_DISCOUNT, decemberEventPeriod,
-                new DayOfWeekDiscountStrategy(MenuCategory.DESSERT, 2_023, weekdayIndex),
+                new DayOfWeekDiscountStrategy(MenuCategory.DESSERT, EventConstants.WEEKDAY_DISCOUNT_AMOUNT.getValue(),
+                        weekdayIndex),
                 null));
 
         events.add(new Event(EventType.WEEKEND_DISCOUNT, decemberEventPeriod,
-                new DayOfWeekDiscountStrategy(MenuCategory.MAIN, 2_023, weekendIndex),
+                new DayOfWeekDiscountStrategy(MenuCategory.MAIN, EventConstants.WEEKEND_DISCOUNT_AMOUNT.getValue(),
+                        weekendIndex),
                 null));
 
         events.add(new Event(EventType.SPECIAL_DISCOUNT, decemberEventPeriod,
-                new SpecialDiscountStrategy(1_000, specialDates),null));
+                new SpecialDiscountStrategy(EventConstants.SPECIAL_DISCOUNT_AMOUNT.getValue(), specialDates), null));
 
         events.add(new Event(EventType.GIFT_REWARD, decemberEventPeriod,
-                null, new GiftRewardStrategy(120_000)));
+                null, new GiftRewardStrategy(EventConstants.GIFT_REWARD_CHAMPAGNE_THRESHOLD.getValue())));
 
-        events.add(new Event(EventType.BADGE_REWARD, decemberEventPeriod,
-                null, new BadgeRewardStrategy(5_000, 10_000, 20_000)));
+        events.add(new Event(EventType.BADGE_REWARD, decemberEventPeriod, null,
+                new BadgeRewardStrategy(EventConstants.BADGE_REWARD_STAR_THRESHOLD.getValue(),
+                        EventConstants.BADGE_REWARD_TREE_THRESHOLD.getValue(),
+                        EventConstants.BADGE_REWARD_SANTA_THRESHOLD.getValue())));
 
         return events;
     }
