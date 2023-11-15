5e5f8659c78e5a43553482ec63a45933bdbb02e6
leemiyeon <lemingul7@gmail.com>
2023-11-15 15:36:44 +0900

    refactor: 주문 생성 및 정보 저장 로직 리팩토링 - Order: 불변 객체로 리팩토링 - FinalizedOrder: 주문과 이벤트 정보를 담은 객체 구현

--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -1,26 +1,16 @@
 package christmas.domain.order;
 
-import christmas.domain.constants.event.EventType;
-import christmas.domain.constants.event.reward.Badge;
-import christmas.domain.constants.event.reward.Gift;
 import christmas.domain.menu.MenuItem;
-import java.util.EnumMap;
-import java.util.LinkedHashSet;
+import java.util.Collections;
 import java.util.Map;
-import java.util.Set;
 
 public class Order {
     private final int visitDay;
     private final Map<MenuItem, Integer> items;
-    private final int orderTotal;
-    private final Map<EventType, Integer> discountDetails = new EnumMap<>(EventType.class);
-    private final Set<Gift> gifts = new LinkedHashSet<>();
-    private final Set<Badge> badges = new LinkedHashSet<>();
 
-    public Order(int visitDay, Map<MenuItem, Integer> items, int orderTotal) {
+    public Order(int visitDay, Map<MenuItem, Integer> items) {
         this.visitDay = visitDay;
         this.items = items;
-        this.orderTotal = orderTotal;
     }
 
     public int getVisitDay() {
@@ -28,51 +18,12 @@ public class Order {
     }
 
     public Map<MenuItem, Integer> getItems() {
-        return items;
+        return Collections.unmodifiableMap(items);
     }
 
-    public int getOrderTotal() {
-        return orderTotal;
-    }
-
-    public Map<EventType, Integer> getDiscountDetails() {
-        return discountDetails;
-    }
-
-    public int getDiscountAmount() {
-        return discountDetails.values().stream()
-                .mapToInt(Integer::intValue)
+    public int calculateOrderTotal() {
+        return items.entrySet().stream()
+                .mapToInt(entry -> entry.getKey().getPrice() * entry.getValue())
                 .sum();
-    }
-
-    public Set<Gift> getGifts() {
-        return gifts;
-    }
-
-    public Set<Badge> getBadges() {
-        return badges;
-    }
-
-    public void applyDiscount(EventType eventType, int discount) {
-        discountDetails.merge(eventType, discount, Integer::sum);
-    }
-
-    public void addGift(Gift gift) {
-        gifts.add(gift);
-    }
-
-    public void addBadge(Badge badge) {
-        badges.add(badge);
-    }
-
-    public int getExpectedPaymentAfterDiscount() {
-        return orderTotal - getDiscountAmount();
-    }
-
-    public int calculateTotalBenefitAmount() {
-        int giftsTotalPrice = gifts.stream()
-                .mapToInt(Gift::getPrice)
-                .sum();
-        return getDiscountAmount() + giftsTotalPrice;
     }
 }
