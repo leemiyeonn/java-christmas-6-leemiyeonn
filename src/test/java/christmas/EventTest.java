7a98c28cc0590b91ba622f1937f88b86e06178b5
leemiyeon <lemingul7@gmail.com>
2023-11-15 19:59:40 +0900

    test: 테스트 리팩토링

--- src/test/java/christmas/EventTest.java
+++ src/test/java/christmas/EventTest.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class EventTest {
+class EventTest {
     private Order testOrder;
     private EventService eventService;
 
--- src/test/java/christmas/ValidatorTest.java
+++ src/test/java/christmas/ValidatorTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class ValidatorTest {
+class ValidatorTest {
     @Nested
     class DateValidatorTests {
         private DateValidator dateValidator;
