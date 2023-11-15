abdc2e4c0034593368a64f2f8e59eb7fa1950783
leemiyeon <lemingul7@gmail.com>
2023-11-15 15:44:09 +0900

    refactor: 주문 생성 및 정보 저장 로직 리팩토링 - 주문 생성 로직 OrderService -> Controller 로 이동

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -1,23 +1,37 @@
 package christmas.controller;
 
+import christmas.domain.menu.MenuItem;
+import christmas.domain.order.FinalizedOrder;
 import christmas.domain.order.Order;
 import christmas.exception.ExceptionHandler;
 import christmas.exception.RetryExceptionHandler;
+import christmas.service.EventService;
 import christmas.service.OrderService;
 import christmas.utils.validator.DateValidator;
 import christmas.utils.validator.OrderValidator;
 import christmas.view.input.InputView;
+import christmas.view.output.OutputView;
+import java.util.Map;
 import java.util.function.Supplier;
 
 public class Controller {
     private final OrderService orderService;
+    private final EventService eventService;
 
-    public Controller(OrderService orderService) {
+    public Controller(OrderService orderService, EventService eventService) {
         this.orderService = orderService;
+        this.eventService = eventService;
     }
 
     public void run() {
+        OutputView.printStart();
+
         int visitDay = createEventDate();
+        Order originalOrder = createOrder(visitDay);
+
+        FinalizedOrder finalizedOrder = eventService.applyEventDiscountsAndRewards(originalOrder);
+
+        OutputView.printResults(originalOrder, finalizedOrder);
     }
 
     private int createEventDate() {
@@ -39,6 +53,7 @@ public class Controller {
     }
 
     private Order processValidOrder(int visitDay, String validOrder) {
-        return orderService.createOrderFromInput(visitDay, validOrder);
+        Map<MenuItem, Integer> orderDetails = orderService.parseOrderDetails(validOrder);
+        return new Order(visitDay, orderDetails);
     }
 }
--- src/main/java/christmas/service/OrderService.java
+++ src/main/java/christmas/service/OrderService.java
@@ -1,6 +1,5 @@
 package christmas.service;
 
-import christmas.domain.order.Order;
 import christmas.domain.menu.MenuItem;
 import java.util.EnumMap;
 import java.util.List;
@@ -8,14 +7,7 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 public class OrderService {
-    public Order createOrderFromInput(int visitDay, String validOrderInput) {
-        Map<MenuItem, Integer> orderDetails = parseOrderDetails(validOrderInput);
-        int totalOrderPrice = calculateTotalPrice(orderDetails);
-
-        return new Order(visitDay, orderDetails, totalOrderPrice);
-    }
-
-    private Map<MenuItem, Integer> parseOrderDetails(String orderInput) {
+    public Map<MenuItem, Integer> parseOrderDetails(String orderInput) {
         List<String> orderedItems = Stream.of(orderInput.split(","))
                 .map(String::trim)
                 .toList();
@@ -35,12 +27,5 @@ public class OrderService {
         }
 
         return orderDetails;
-    }
-
-    private int calculateTotalPrice(Map<MenuItem, Integer> orderDetails) {
-        return orderDetails.entrySet()
-                .stream()
-                .mapToInt(entry -> entry.getKey().getPrice() * entry.getValue())
-                .sum();
     }
 }
